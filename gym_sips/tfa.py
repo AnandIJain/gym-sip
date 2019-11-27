7eb73fa5e7639f099c7c1d8a3bdc53636dd69879
anandijain <anandj@uchicago.edu>
2019-11-27 17:07:29 -0600

    tfa.py imports Sips-v0 using suite_gym, added requirements.txt

--- gym_sips/tfa.py
+++ gym_sips/tfa.py
@@ -1,7 +1,20 @@
+import random
+
 import gin
 import gym
 
-from tf_agents.environments import gym_wrapper
-from tf_agents.environments import wrappers
+from tf_agents.environments import suite_gym
 
 import gym_sips
+
+MAX_STEPS = 10
+
+if __name__ == "__main__":
+    env = suite_gym.load('Sips-v0', max_episode_steps=MAX_STEPS)
+    for i in range(MAX_STEPS):
+        o, r, d, i = env.step(random.randint(0, 2))
+        print('action_spec:', env.action_spec())
+        print('time_step_spec.observation:', env.time_step_spec().observation)
+        print('time_step_spec.step_type:', env.time_step_spec().step_type)
+        print('time_step_spec.discount:', env.time_step_spec().discount)
+        print('time_step_spec.reward:', env.time_step_spec().reward)
